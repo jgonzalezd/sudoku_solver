20f22f607e089cbeaef957fef6ad55e355a370ec
Julian Gonzalez <juliangonzalez.code@gmail.com>
2017-03-18 15:59:56 -0500

    removed unnecessary files

--- game.rb
+++ game.rb
@@ -55,7 +55,7 @@ private
 
   def get_block(board, position)
     index_i = (position.first / 3) * 3
-    index_j = (position.last / 3) * 3
+    index_j = (position.last  / 3) * 3
     board.minor(index_i..(index_i+2), index_j..(index_j+2))
   end
 
@@ -63,40 +63,3 @@ private
     board.find_index(0).nil?
   end
 end # class
-
-
-
-
-
-
-
-
-
-
-
-
-def solve_game(board)
-  for i in 0..8
-    available_row_choices = number_set - board.row(i).to_a
-    for j in 0..8
-      unless board[i,j] != 0
-        available_col_choices = number_set - board.column(j).to_a
-        available_pos_choices = available_row_choices & available_col_choices
-        if available_pos_choices.size > 0
-          available_pos_choices.each do |choice|
-            update_board([i,j], choice)
-            if solve_game(game_board.board)
-              available_row_choices.delete(choice)
-              return true
-            else
-              return false
-            end #if
-          end #each
-        else
-          byebug
-        end # if
-        p game_board.board
-      end #unless
-    end # for j
-  end # for i
-end # solve_game
